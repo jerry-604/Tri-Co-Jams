6341f55c0e644af641cad785bb9121ff51478a6a
Thomas Makin <halorocker89@gmail.com>
2022-11-05 16:10:09 -0400

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,14 +22,21 @@ export default function Home() {
 
       <main className={styles.main}>
         <Header/>
-        <Container>
-            <Row height="50vh">
+        <style type="text/css">
+                        {`
+                        .mt-7 {
+                            margin-top:20vh;
+                        }
+                        `}
+                    </style>
+        <Container className=" mt-7 align-items-end">
+            <Row height="50vh"  className=" mt-5">
                 <Col height="50vh" className="mt-4 d-flex justify-content-end" xs={2} s={2} md={4} lg={5} >
                     <Image className="float-right" src="/treble.svg" alt="Treble cleff vector art" width="100" height="200" />
                 </Col>
-                <Col height="50vh" className="mr-4" fluid="s" s={8} md={6} lg={4}>
+                <Col height="50vh" className="mr-4 d-flex align-items-center" fluid="s" s={8} md={6} lg={4}>
                     <h4 className="mt-4 mr-4">
-                        TriCo Jams is a student-run,
+                      TriCo Jams is a student-run,
                         passion-driven site for finding
                         jam sessions in the Swarthmore/
                         Haverford/Bryn Mawr community.
