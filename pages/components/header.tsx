2a3ef764abe02ecfe62a17cbc24adfd28e22c548
Thomas Makin <halorocker89@gmail.com>
2022-11-05 01:49:03 -0400

    style: home: setup part of front page

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -1,11 +1,10 @@
-import {Nav, Navbar, Container, NavDropdown} from 'react-bootstrap';  
+import {Nav, Navbar, Container, NavDropdown} from 'react-bootstrap';
 
 function Header() {
     return (
         <Navbar className="nav justify-content-center" id="bar" bg="primary" expand="md">  
         <Container className="nav justify-content-center" id="container">  
-          <Navbar.Brand id="brand" href="#home">TriCo Jams</Navbar.Brand>  
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />  
+          <Navbar.Brand className="navbar-brand m-0 p-0 text-secondary" href="#home">TriCo Jams</Navbar.Brand> 
         </Container>  
       </Navbar>  
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from './components/header.tsx'
+import Header from './components/header'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
@@ -14,6 +17,27 @@ export default function Home() {
 
       <main className={styles.main}>
         <Header/>
+
+        <Container>
+            <Row width="100%">
+                <Col className="mt-4 d-flex justify-content-end" xs={4} >
+                    <Image className="float-right" src="/treble.svg" alt="Treble cleff vector art" width="100" height="200" />
+                </Col>
+                <Col xs={4}>
+                    <h4 className="mt-4">
+                        TriCo Jams is a student-run,
+                        passion-driven site for finding
+                        jam sessions in the Swarthmore/
+                        Haverford/Bryn Mawr community.
+                    </h4>
+                </Col>
+            </Row>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
       </main>
     </div>
   )
