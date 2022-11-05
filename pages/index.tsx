fd400a3d56a968afefb562079b752af9198df1a0
Thomas Makin <halorocker89@gmail.com>
2022-11-05 12:00:41 -0400

    WIP: buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from './components/header'
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -19,12 +20,12 @@ export default function Home() {
         <Header/>
 
         <Container>
-            <Row width="100%">
-                <Col className="mt-4 d-flex justify-content-end" xs={4} >
+            <Row>
+                <Col className="mt-4 d-flex justify-content-end" xs={2} s={2} md={4} lg={5} >
                     <Image className="float-right" src="/treble.svg" alt="Treble cleff vector art" width="100" height="200" />
                 </Col>
-                <Col xs={4}>
-                    <h4 className="mt-4">
+                <Col className="mr-4" fluid="s" s={8} md={6} lg={4}>
+                    <h4 className="mt-4 mr-4">
                         TriCo Jams is a student-run,
                         passion-driven site for finding
                         jam sessions in the Swarthmore/
@@ -32,10 +33,13 @@ export default function Home() {
                     </h4>
                 </Col>
             </Row>
-            <Row>
-                <Col>1 of 3</Col>
-                <Col>2 of 3</Col>
-                <Col>3 of 3</Col>
+            <Row className="mt-4 justify-content-center">
+                <Col className="mt-4 justify-content-center">
+                    <Button className="justify-content-center btn-xxl" variant="primary">Host</Button>
+                </Col>
+                <Col className="mt-4 justify-content-center">
+                    <Button className="justify-content-center btn-xxl" variant="primary">Join</Button>
+                </Col>
             </Row>
         </Container>
       </main>
