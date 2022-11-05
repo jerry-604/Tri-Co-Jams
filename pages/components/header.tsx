e1c1ecaa84e112f484df392b6cf1d47ed32daaff
Thomas Makin <halorocker89@gmail.com>
2022-11-04 22:12:36 -0400

    Update navbar

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -4,21 +4,8 @@ function Header() {
     return (
         <Navbar bg="primary" expand="md">  
         <Container>  
-          <Navbar.Brand href="#home">Navbar Brand</Navbar.Brand>  
+          <Navbar.Brand id="brand" href="#home">TriCo Jams</Navbar.Brand>  
           <Navbar.Toggle aria-controls="basic-navbar-nav" />  
-          <Navbar.Collapse id="basic-navbar-nav">  
-            <Nav className="me-auto">  
-              <Nav.Link href="#home">Home</Nav.Link>  
-              <Nav.Link href="#link">Link</Nav.Link>  
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">  
-                <NavDropdown.Item href="#action/3.1">Dropdown Item 1</NavDropdown.Item>  
-                <NavDropdown.Item href="#action/3.2">Dropdown Item 2</NavDropdown.Item>  
-                <NavDropdown.Item href="#action/3.3">Dropdown Item 3</NavDropdown.Item>  
-                <NavDropdown.Divider />  
-                <NavDropdown.Item href="#action/3.4">Another Item</NavDropdown.Item>  
-              </NavDropdown>  
-            </Nav>  
-          </Navbar.Collapse>  
         </Container>  
       </Navbar>  
     );
